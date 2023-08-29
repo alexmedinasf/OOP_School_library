8a32adda5203c1eefa086bb257bdd5114a9a9c32
Alexandre <alexsales001@hotmail.com>
2023-08-29 13:04:53 -0300

    removed id, changed notation

--- student.rb
+++ student.rb
@@ -1,15 +1,13 @@
-require_relative 'person'
-
 # This file defines the Student class, representing students associated with the school library app.
 class Student < Person
-  attr_accessor :classroom
-
-  def initialize(id:, age:, classroom:, parent_permission: true, name: 'Unknown')
-    super(id: id, age: age, parent_permission: parent_permission, name: name)
-    @classroom = classroom
+    attr_accessor :classroom
+  
+    def initialize(age, classroom:, parent_permission: true, name: 'Unknown')
+      super(age: age, parent_permission: parent_permission, name: name)
+      @classroom = classroom
+    end
+  
+    def play_hooky
+      '¯\\(ツ)/¯'
+    end
   end
-
-  def play_hooky
-    '¯\\(ツ)/¯'
-  end
-end
