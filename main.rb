232a9b8e8583ac64fd9256cccbfd1efb9a165bb2
Alexandre <alexsales001@hotmail.com>
2023-08-29 14:40:50 -0300

    linters

--- main.rb
+++ main.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # main.rb
 require_relative 'person'
 require_relative 'capitalize_decorator'
--- person.rb
+++ person.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # person.rb
 class Person < Nameable
   attr_reader :name, :age
