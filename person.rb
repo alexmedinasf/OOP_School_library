2a7603ddbe42a7be5cfa832db744bd05a31f63df
Alexandre <alexsales001@hotmail.com>
2023-08-29 13:43:50 -0300

    correct_name method

--- person.rb
+++ person.rb
@@ -1,24 +1,14 @@
 # This file defines the Person class, representing individuals associated with the school library app.
-class Person
-  attr_accessor :name, :age
-  attr_reader :id
-
-  def initialize(age:, parent_permission: true, name: 'Unknown')
-    @id = Random.rand(1..1000)
-    @name = name
-    @age = age
-    @parent_permission = parent_permission
+class Person < Nameable
+    attr_reader :name, :age
+  
+    def initialize(age, name)
+      @age = age
+      @name = name
+    end
+  
+    def correct_name
+      name
+    end
   end
-
-  private
-
-  def of_age?
-    @age >= 18
-  end
-
-  public
-
-  def can_use_services?
-    @age >= 18 || @parent_permission
-  end
-end
+  
