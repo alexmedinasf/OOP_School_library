3fd1c1431a8d0f1550b43eec8258584df459b67f
Alexandre <alexsales001@hotmail.com>
2023-08-29 13:49:22 -0300

    define base decorator class

---
+++ base_decorator.rb
@@ -0,0 +1,11 @@
+# base_decorator.rb
+class BaseDecorator < Nameable
+    def initialize(nameable)
+      @nameable = nameable
+    end
+  
+    def correct_name
+      @nameable.correct_name
+    end
+  end
+  
