a8589c4d228ac6c441fa4709a2058a5c938f59d4
Alexandre <alexsales001@hotmail.com>
2023-08-31 18:34:25 -0300

    modify student class

--- student.rb
+++ student.rb
@@ -1,15 +1,10 @@
-# frozen_string_literal: true
-
 # This file defines the Student class, representing students associated with the school library app.
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, classroom:, parent_permission: true, name: 'Unknown')
-    super(age: age, parent_permission: parent_permission, name: name)
+  def initialize(age, name, classroom)
+    super(age, name)
     @classroom = classroom
-  end
-
-  def play_hooky
-    '¯\\(ツ)/¯'
+    classroom.add_student(self)
   end
 end
